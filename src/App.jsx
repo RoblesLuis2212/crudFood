df3c672d94bccda2984346028b47a4804ba4ece4
Luis Robles <luisgeremiasrobles@gmail.com>
2025-08-28 20:07:26 -0300

    Agregamos la lista de rutas en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import Menu from "./components/pages/shared/Menu";
+import Footer from "./components/pages/shared/Footer";
+import Inicio from "./components/pages/Inicio";
+import Login from "./components/pages/Login";
+import DetalleProducto from "./components/pages/DetalleProducto";
+import Administrador from "./components/pages/Administrador";
+import Error404 from "./components/pages/Error404";
+import { BrowserRouter, Routes, Route } from "react-router";
+BrowserRouter;
+
 function App() {
   return (
     <>
-      <h1>Hola mundo!</h1>
+      <BrowserRouter>
+        <Menu></Menu>
+        <Routes>
+          <Route path="/" element={<Inicio></Inicio>}></Route>
+          <Route
+            path="/detalle"
+            element={<DetalleProducto></DetalleProducto>}
+          ></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route
+            path="/administrador"
+            element={<Administrador></Administrador>}
+          ></Route>
+          <Route path="*" element={<Error404></Error404>}></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/pages/shared/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Esto es el navbar</h2>
+    </div>
+  );
+};
+
+export default Menu;
