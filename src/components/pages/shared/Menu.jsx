4c55894018f19d0d4b4e0704cf31378db35eacf6
Luis Robles <luisgeremiasrobles@gmail.com>
2025-08-28 20:21:51 -0300

    Instalamos bootstrap icons

--- src/components/pages/shared/Menu.jsx
+++ src/components/pages/shared/Menu.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-
+import { Navbar, Container, Nav } from "react-bootstrap";
 const Menu = () => {
   return (
     <div>
-      <h2>Esto es el navbar</h2>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Inicio</Nav.Link>
+
+              <Nav.Link href="#link">Administrador</Nav.Link>
+              <Nav.Link href="#link">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
