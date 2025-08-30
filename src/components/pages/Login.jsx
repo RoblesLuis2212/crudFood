33753345dfd38adc453c2a21b7a4878fca375643
Luis Robles <luisgeremiasrobles@gmail.com>
2025-08-30 11:11:22 -0300

    Se agrego el login y se lo adapto para los distintos tamaños de dispositivos

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import { Button, Row, Col, Form } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Card className="mt-4">
+      <Row xs={1} md={2}>
+        <Col xs={{ order: 2 }} md={{ order: 1 }}>
+          <Card.Body>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="juanperez@gmail.com" />
+                <Form.Text className="text-danger">mensaje de error</Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="ingresa tu contraseña"
+                />
+                <Form.Text className="text-danger">mensaje de error</Form.Text>
+              </Form.Group>
+              <Button variant="warning" type="submit">
+                Iniciar Sesión
+              </Button>
+            </Form>
+          </Card.Body>
+        </Col>
+        <Col xs={{ order: 1 }} md={{ order: 2 }}>
+          <img
+            src="https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg"
+            alt="comida"
+            className="img-fluid h-100"
+          />
+        </Col>
+      </Row>
+    </Card>
   );
 };
 
