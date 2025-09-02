914f4a7001e9c665eb06ce64c639b2ad092e89ca
Luis Robles <luisgeremiasrobles@gmail.com>
2025-09-02 10:57:14 -0300

    Se maqueto el componente detalleProducto

--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import { Button, Row, Col, Form } from "react-bootstrap";
 
 const DetalleProducto = () => {
   return (
-    <div>
-      <h1>Detalle del producto</h1>
-    </div>
+    <section className="container border mt-5">
+      <Row xs={1} md={2}>
+        <Col className="p-0">
+          <img
+            src="https://images.pexels.com/photos/2619970/pexels-photo-2619970.jpeg"
+            alt="Pizza"
+            className="img-fluid h-100"
+          />
+        </Col>
+        <Col>
+          <h3 className="fontTiza mt-3">Pizza Margarita Clasíca</h3>
+          <hr />
+          <p className="fontTiza">
+            Nuestra pizza margarita artesanal hecha con casa madre, salsa de
+            tomate casera, queso mozzarella de búfala premium y hojas frescas de
+            albahaca. Horneada en horno de piedra para obtener ese crujiente
+            perfecto.
+          </p>
+          <p className="fontTiza">Categoria: comida</p>
+          <p className="fontTiza mt-0">Precio: $4500</p>
+        </Col>
+      </Row>
+    </section>
   );
 };
 
